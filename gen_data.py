d1062fae24ea31274b37bf89101a7ee4b12a722b
Nikita Chepurenko <nikita@Nikita.local>
2022-04-30 10:20:22 +0300

    fix gen_data

--- gen_data.py
+++ gen_data.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 from typing import List
 
@@ -9,13 +10,14 @@ path_to_js_file: str = os.path.join(os.getcwd(), 'static', 'scripts', 'variables
 df: pd.DataFrame = pd.read_csv(path_to_data, skiprows=2)
 df.rename(columns={'Unnamed: 0': 'year'}, inplace=True)
 
-labels: List[str] = df.columns[1:].to_list()
+labels: List[str] = df.columns[1:].to_list() + ['Другие']
 data_by_one: List[int] = df[labels[0]].to_list()
 data_by_two: List[int] = df[labels[1]].to_list()
+data_by_another: List[int] = [*np.array(100) - (np.array(data_by_one) + np.array(data_by_two))]
 
 label: str = f"let labels = {labels};"
 label_name: str = "let labelName = 'Доля геополитических блоков';"
-data: str = f"let data = [{sum(data_by_one)}, {sum(data_by_two)}];"
+data: str = f"let data = [{data_by_one}, {data_by_two}, {data_by_another}];"
 result = '// gen by python\n{}\n{}\n{}\n// py data gen'
 
 
--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,23 +1,128 @@
-let ourPie = $("#ourPie");
+let ourPie1 = $("#ourPie1");
+let ourPie2 = $("#ourPie2");
+let ourPie3 = $("#ourPie3");
+let ourPie4 = $("#ourPie4");
 
 
-const chartPie = new Chart(ourPie, {
+new Chart(ourPie1, {
     type: 'doughnut', /* pie */
     data: {
         labels: labels,
         datasets: [
             {
                 label: labelName,
-                data: data,
+                data: [data[0][0], data[1][0], data[2][0]],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.3)',
-                    'rgba(54, 162, 235, 0.3)'
+                    'rgba(54, 162, 235, 0.3)',
+                    'rgba(219,133,20,0.3)'
                 ],
                 borderWidth: 1,
                 borderRadius: 7,
                 borderColor: [
                     'rgba(255, 99, 132, 0.9)',
-                    'rgba(54, 162, 235, 0.9)'
+                    'rgba(54, 162, 235, 0.9)',
+                    'rgba(219,133,20,0.9)'
+
+                ]
+            }
+        ]
+    },
+
+    options: {
+        plugins: {
+            legend: {
+                display: true,
+                position: "left",
+            }
+        }
+    }
+});
+new Chart(ourPie2, {
+    type: 'doughnut', /* pie */
+    data: {
+        labels: labels,
+        datasets: [
+            {
+                label: labelName,
+                data: [data[0][1], data[1][1], data[2][1]],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.3)',
+                    'rgba(54, 162, 235, 0.3)',
+                    'rgba(219,133,20,0.3)'
+                ],
+                borderWidth: 1,
+                borderRadius: 7,
+                borderColor: [
+                    'rgba(255, 99, 132, 0.9)',
+                    'rgba(54, 162, 235, 0.9)',
+                    'rgba(219,133,20,0.9)'
+                ]
+            }
+        ]
+    },
+
+    options: {
+        plugins: {
+            legend: {
+                display: true,
+                position: "left",
+            }
+        }
+    }
+});
+new Chart(ourPie3, {
+    type: 'doughnut', /* pie */
+    data: {
+        labels: labels,
+        datasets: [
+            {
+                label: labelName,
+                data: [data[0][2], data[1][2], data[2][2]],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.3)',
+                    'rgba(54, 162, 235, 0.3)',
+                    'rgba(219,133,20,0.3)'
+                ],
+                borderWidth: 1,
+                borderRadius: 7,
+                borderColor: [
+                    'rgba(255, 99, 132, 0.9)',
+                    'rgba(54, 162, 235, 0.9)',
+                    'rgba(219,133,20,0.9)'
+                ]
+            }
+        ]
+    },
+
+    options: {
+        plugins: {
+            legend: {
+                display: true,
+                position: "left",
+            }
+        }
+    }
+});
+new Chart(ourPie4, {
+    type: 'doughnut', /* pie */
+    data: {
+        labels: labels,
+        datasets: [
+            {
+                label: labelName,
+                data: [data[0][3], data[1][3], data[2][3]],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.3)',
+                    'rgba(54, 162, 235, 0.3)',
+                    'rgba(219,133,20,0.3)'
+                ],
+                borderWidth: 1,
+                borderRadius: 7,
+                borderColor: [
+                    'rgba(255, 99, 132, 0.9)',
+                    'rgba(54, 162, 235, 0.9)',
+                    'rgba(219,133,20,0.9)'
                 ]
             }
         ]
